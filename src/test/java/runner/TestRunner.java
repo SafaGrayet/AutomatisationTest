69d86fffcb998ffd7b331501ab0984a7589d1151
SafaGrayet <37809055+SafaGrayet@users.noreply.github.com>
2018-05-22 17:20:04 +0200

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,11 +1,12 @@
 package runner;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"C:\\Users\\PC-ASUS\\IdeaProjects\\CucumberBasics\\src\\test\\java\\features"}, glue ="Steps" )
-public class TestRunner {
+@CucumberOptions(features = {"src/test/java/features"}, glue ="Steps" )
+public class TestRunner extends AbstractTestNGCucumberTests {
 
 
 
